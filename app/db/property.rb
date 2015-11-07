7441311f0b0ee83094a5784181495510669a7aa4
rickwargo <github@epicminds.com>
2015-11-06 22:17:27 -0500

    revert to original records to complete SQL

--- app/db/property.rb
+++ app/db/property.rb
@@ -5,14 +5,10 @@ module DB
   class Property < MontcoBase
     def initialize
       @table = 'properties'
-      # @records_to_complete_sql = %Q{
-      #   SELECT parcel_id
-      #   FROM #{@table}
-      #   WHERE municipality IS NULL}
       @records_to_complete_sql = %Q{
         SELECT parcel_id
         FROM #{@table}
-        WHERE land_use_code = 0}
+        WHERE municipality IS NULL}
     end
   end
 
